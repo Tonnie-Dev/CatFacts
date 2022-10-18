29ab488ae91a39cbaea20d002bf816991e98cdba
Tonnie <vontonnie@gmail.com>
2022-10-18 21:34:24 +0300

    Import ic_error vector drawable

---
+++ app/src/main/java/com/uxstate/catfacts/presentation/screens/overview_screen/components/ErrorItem.kt
@@ -0,0 +1,14 @@
+package com.uxstate.catfacts.presentation.screens.overview_screen.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ErrorItem() {
+    Box(modifier = Modifier.fillMaxSize()){
+
+        
+    }
+}
