13e66c78f9fde7621b597c04cda01079bfd1fd7a
Tonnie <vontonnie@gmail.com>
2022-10-18 09:50:43 +0300

    Add CatFactDTO.toModel() mapper function

--- app/src/main/java/com/uxstate/catfacts/data/mapper/Mapper.kt
+++ app/src/main/java/com/uxstate/catfacts/data/mapper/Mapper.kt
@@ -1,14 +1,25 @@
 package com.uxstate.catfacts.data.mapper
 
 import com.uxstate.catfacts.data.remote.dto.ApiResponseDTO
+import com.uxstate.catfacts.data.remote.dto.CatFactDTO
 import com.uxstate.catfacts.domain.model.ApiResponse
+import com.uxstate.catfacts.domain.model.CatFact
 
 
-//from dto to model
+//fact from dto to model
+
+fun CatFactDTO.toModel():CatFact {
+
+    return CatFact( fact = this.fact, length = this.length)
+}
+
+//fact from model to dto
+
+//response from dto to model
 fun ApiResponseDTO.toModel():ApiResponse {
 
 
     return ApiResponse(data = this.data, currentPage = this.currentPage, lastPage = this.lastPage)
 }
 
-//from model to dto
+//response from model to dto
