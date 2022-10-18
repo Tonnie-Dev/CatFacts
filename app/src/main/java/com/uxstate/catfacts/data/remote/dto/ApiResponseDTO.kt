7e2cf50ae5db43b713008202136657247bff78d2
Tonnie <vontonnie@gmail.com>
2022-10-18 09:12:09 +0300

    Refactor CatFileDTO to ApiResponseDTO

---
+++ app/src/main/java/com/uxstate/catfacts/data/remote/dto/ApiResponseDTO.kt
@@ -0,0 +1,14 @@
+package com.uxstate.catfacts.data.remote.dto
+
+
+import com.squareup.moshi.Json
+
+data class ApiResponseDTO(
+    @Json(name = "current_page")
+    val currentPage: Int,
+    @Json(name = "data")
+    val data: List<Data>,
+    @Json(name = "last_page")
+    val lastPage: Int,
+   
+)
