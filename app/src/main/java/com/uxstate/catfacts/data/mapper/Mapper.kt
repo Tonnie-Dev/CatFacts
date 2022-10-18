fb4a333d656768adcfdb84d699824074050c6510
Tonnie <vontonnie@gmail.com>
2022-10-18 09:53:53 +0300

    Add ApiResponseDTO.toModel() mapper extension function

--- app/src/main/java/com/uxstate/catfacts/data/mapper/Mapper.kt
+++ app/src/main/java/com/uxstate/catfacts/data/mapper/Mapper.kt
@@ -7,22 +7,26 @@ import com.uxstate.catfacts.domain.model.CatFact
 
 
 //fact from dto to model
-fun CatFactDTO.toModel():CatFact {
+fun CatFactDTO.toModel(): CatFact {
 
-    return CatFact( fact = this.fact, length = this.length)
+    return CatFact(fact = this.fact, length = this.length)
 }
 
 //fact from model to dto
-fun CatFact.toDto():CatFactDTO {
+fun CatFact.toDto(): CatFactDTO {
 
-    return CatFactDTO( fact = this.fact, length = this.length)
+    return CatFactDTO(fact = this.fact, length = this.length)
 }
 
 //response from dto to model
-fun ApiResponseDTO.toModel():ApiResponse {
+fun ApiResponseDTO.toModel(): ApiResponse {
 
 
-    return ApiResponse(data = this.data, currentPage = this.currentPage, lastPage = this.lastPage)
+    return ApiResponse(
+            data = this.data.map { it.toModel() },
+            currentPage = this.currentPage,
+            lastPage = this.lastPage
+    )
 }
 
 //response from model to dto
