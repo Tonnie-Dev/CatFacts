fa930a10eab8002e18df4c26fde5debade57aa6a
Tonnie <vontonnie@gmail.com>
2022-10-18 22:14:21 +0300

    Specify Application class name in the manifest file

--- app/src/main/java/com/uxstate/catfacts/presentation/MainActivity.kt
+++ app/src/main/java/com/uxstate/catfacts/presentation/MainActivity.kt
@@ -12,7 +12,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.catfacts.presentation.screens.overview_screen.OverviewScreen
 import com.uxstate.catfacts.presentation.ui.theme.CatFactsTheme
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
