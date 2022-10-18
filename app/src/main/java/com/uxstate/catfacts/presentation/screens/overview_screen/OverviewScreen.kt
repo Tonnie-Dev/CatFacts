2b8a3ed84646c37347dd199f54c8281fb2ef62ad
Tonnie <vontonnie@gmail.com>
2022-10-18 21:54:46 +0300

    Add LoadItemExtension function on OverviewScreen

--- app/src/main/java/com/uxstate/catfacts/presentation/screens/overview_screen/OverviewScreen.kt
+++ app/src/main/java/com/uxstate/catfacts/presentation/screens/overview_screen/OverviewScreen.kt
@@ -1,11 +1,15 @@
 package com.uxstate.catfacts.presentation.screens.overview_screen
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.uxstate.catfacts.presentation.screens.overview_screen.components.LoadingItem
 import com.uxstate.catfacts.presentation.screens.overview_screen.components.TopRow
 
 @Composable
@@ -19,9 +23,30 @@ fun OverviewScreen(viewModel: CatViewModel = hiltViewModel()) {
         TopRow(lastPage = 3, modifier = Modifier.fillMaxWidth()) {
 
         }
-    }) {
+    }) { values ->
 
+        LazyColumn(contentPadding = values,
+                content = {
+                    when(facts.loadState.refresh){
+
+                        is LoadState.Loading -> {}
+                        is LoadState.NotLoading -> {}
+                        is LoadState.Error -> {}
+                    }
+
+
+
+                })
 
 
     }
+}
+
+
+fun LazyListScope.LoadItemExtension(){
+
+  item {
+
+      LoadingItem()
+  }
 }
